927dcdaec70adf9ea9224a7f730648875afb92bf
med80-u <barkaoui.mohammed@yahoo.com>
2025-07-15 02:01:29 +0000

    some modifs test approvals

--- python/tests/test_gilded_rose_approvals.py
+++ python/tests/test_gilded_rose_approvals.py
@@ -1,6 +1,7 @@
 import io
 import sys
-
+import os 
+#sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
 from approvaltests import verify
 from texttest_fixture import main
 
